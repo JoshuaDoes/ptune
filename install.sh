8d4d861df08ea4b6769adfdd5145b7e6b484e964
JoshuaDoes <joshuadoespersonalstuff@gmail.com>
2024-12-09 21:00:32 -0500

    ptune: Release v1.3.0
    Fixes support for hOS December 2024. Your mileage may vary. May break on November 2024 and earlier versions, won't test. May break on many other ROMs, won't fix until v2.0.0. All that remains are bugfixes until that update.
    
    init:
    - Drop hardcoded Zygote32 definition (not used on Pixel)
    - Add support for mediametrics
    - Fix groups for audioserver
    - Drop sockets from surfaceflinger
    - Fix groups and drop accidental extra lines for EdgeTPU definition
    - Migrate gralloc from V1 to V2
    - Set ioprio rt 4 for all services
    
    service:
    - Schedule child processes to run first (again, just for now?)
    - Revert blocksched to use mq-deadline (Pixel only?)
    - Old (run on big | should idle | group):
    1 | 0 | cam
    0 | 1 | fg
    1 | 1 | rt
    1 | 1 | sys
    0 | 1 | ta
    - New (run on big | should idle | group):
    0 | 0 | cam
    1 | 1 | fg
    1 | 1 | fg_wi
    0 | 1 | rt
    0 | 0 | sys
    1 | 0 | ta
    - Comment out logcat logger, useful to leave in for debugging

--- install.sh
+++ install.sh
@@ -1,9 +1,5 @@
 #!/bin/bash
 
-export VER="v1.3.0"
-export ZIP="ptune-$VER.zip"
-export SRCMOD="$PWD/module"
-
 # Hide output from pushd/popd
 pushd () {
     command pushd "$@" > /dev/null
@@ -12,6 +8,11 @@ popd () {
     command popd "$@" > /dev/null
 }
 
+echo "* Configuring the build environment"
+export VER="v1.2.1"
+export ZIP="ptune-$VER.zip"
+export SRCMOD="$PWD/module"
+
 echo "* Packaging the module"
 pushd "$SRCMOD" >&2
 zip -r -0 -v "$ZIP" . > /dev/null
@@ -19,12 +20,7 @@ popd >&2
 rm "$PWD/$ZIP" >/dev/null 2>&1
 mv "$SRCMOD/$ZIP" "$PWD/$ZIP"
 
-echo "* Done!"
-
-if [ -d /sdcard ]; then
-  rm "/sdcard/$ZIP" >/dev/null 2>&1
-  mv "$ZIP" /sdcard/
-  echo "-> /sdcard/$ZIP"
-else
-  echo "-> $ZIP"
-fi
+echo "* Installing the module"
+echo ""
+sudo magisk --install-module "$ZIP"
+rm "$ZIP"
--- module/service.sh
+++ module/service.sh
@@ -2,8 +2,8 @@
 
 #MODDIR=${0%/*}
 SHDIR="$(dirname $0)"
-MAGISKPFDD=/dev/adb/post-fs-data.d
 MAGISKSD=/dev/adb/service.d
+MAGISKPFDD=/dev/adb/post-fs-data.d
 
 VM=/proc/sys/vm
 KR=/proc/sys/kernel
@@ -37,27 +37,28 @@ fi
 echo 0 > $VM/dirty_writeback_centisecs
 echo 1 > $VM/swappiness
 echo 5 > $VM/vfs_cache_pressure
-echo 0 > $KR/sched_child_runs_first
+echo 1 > $KR/sched_child_runs_first
 
 # Speed up disk access
-blocksched none sda
-blocksched none sdb
-blocksched none sdc
-blocksched none sdd
+blocksched mq-deadline sda
+blocksched mq-deadline sdb
+blocksched mq-deadline sdc
+blocksched mq-deadline sdd
 
 # Allow vendor scheduler groups to migrate to any core if cpuset allows
 echo "2048 2048 2048" > $VS/util_threshold
 
 # Prefer running on big versus prefer to idle
-perfsched 1024 1 0 cam
+perfsched 1024 0 0 cam
 perfsched 1024 0 0 cam_power
 perfsched 1024 0 1 dex2oat
-perfsched 1024 0 1 fg
+perfsched 1024 1 1 fg
+perfsched 1024 1 1 fg_wi
 perfsched 1024 0 1 ota
-perfsched 1024 1 1 rt
+perfsched 1024 0 1 rt
 perfsched 1024 1 1 sf
-perfsched 1024 1 1 sys
-perfsched 1024 0 1 ta
+perfsched 1024 0 0 sys
+perfsched 1024 1 0 ta
 
 # Disable SurfaceFlinger frame dropping, no but for real
 resetprop -d debug.sf.use_phase_offsets_as_durations
@@ -72,9 +73,9 @@ resetprop -d debug.sf.frame_rate_multiple_threshold
 # Allow swap to reach 95% before triggering low memory killer
 resetprop -n ro.lmk.swap_free_low_percentage 5
 
-if [ "$SHDIR" == "$MAGISKSD" ]; then
-  exit 0
-elif [ "$SHDIR" == "$MAGISKPFDD" ]; then
+#if [ "$SHDIR" == "$MAGISKSD" ]; then
+#  exit 0
+#elif [ "$SHDIR" == "$MAGISKPFDD" ]; then
   # Save the boot log!
-  logcat -b all > /data/adb/logcat.log
-fi
+#  logcat -b all > /data/adb/logcat.log
+#fi
